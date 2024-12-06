9834d88b863c3aeeb2bcce68204927442cb0fdc8
adriano3788 <adriano3788@gmail.com>
2024-12-06 13:32:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("O arquivo script.js foi carregado!");
+// Aguarda o carregamento completo do DOM
+document.addEventListener("DOMContentLoaded", () => {
+    // Seleciona o botão e o elemento de resposta
+    const button = document.getElementById("myButton");
+    const responseElement = document.getElementById("response");
+
+    // Adiciona o evento de clique ao botão
+    //button.addEventListener("click", () => {
+       // const horarios = "Horários: Segunda, Quarta e Sexta";
+        //responseElement.textContent = horarios;
+   // });
+});
